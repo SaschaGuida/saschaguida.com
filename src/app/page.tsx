69eabf82bf1ce11f292d73fb13d28c9c85328e44
sak89 <sascha.guida@gmail.com>
2024-11-26 11:21:32 +0100

    portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,87 +7,103 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
+          src="/logo.webp"
+          alt="saschaguida logo"
           width={180}
           height={38}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <div className="cta-container">
+          <h2 className="font">Specialized in building modern web applications with intuitive designs and scalable backends. Passionate about coding and delivering tailored solutions for every project.</h2>
+        </div>
+
 
         <div className={styles.ctas}>
           <a
             className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="#"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
               className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
+              src="/contact.svg"
+              alt="contactme"
               width={20}
               height={20}
             />
-            Deploy now
+            CONTACT ME
           </a>
           <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="#"
             target="_blank"
             rel="noopener noreferrer"
             className={styles.secondary}
           >
-            Read our docs
+            MY PORTFOLIO
           </a>
         </div>
       </main>
       <footer className={styles.footer}>
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.linkedin.com/in/sascha-guida-dev/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
+            className={styles.logo}
             aria-hidden
-            src="/file.svg"
+            src="/linkedin.svg"
             alt="File icon"
             width={16}
             height={16}
           />
-          Learn
+          LINKEDIN
         </a>
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://github.com/SaschaGuida"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
+            className={styles.logo}
             aria-hidden
-            src="/window.svg"
+            src="/github.svg"
             alt="Window icon"
             width={16}
             height={16}
           />
-          Examples
+          GITHUB
         </a>
         <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.instagram.com/saschaguida/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
+            className={styles.logo}
             aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
+            src="/instagram.svg"
+            alt="Instagram icon"
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          INSTAGRAM
+        </a>
+        <a
+          href="https://www.instagram.com/3dprint__it/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            className={styles.logo}
+            aria-hidden
+            src="/print.svg"
+            alt="Print icon"
+            width={16}
+            height={16}
+          />
+          3D PRINT IT
         </a>
       </footer>
     </div>
